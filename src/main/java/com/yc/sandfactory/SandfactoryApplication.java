0cf6d7f688c263faebc4c6a8bc30eaf528b2e4cf
youngc <764991510@qq.com>
2018-05-08 21:37:28 +0800

    init commit

---
+++ src/main/java/com/yc/sandfactory/SandfactoryApplication.java
@@ -0,0 +1,12 @@
+package com.yc.sandfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandfactoryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yc/sandfactory/SandfactoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yc.sandfactory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SandfactoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
