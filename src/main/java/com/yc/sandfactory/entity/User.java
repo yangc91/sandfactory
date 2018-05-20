f6269b7254ecedee00439893f307cd74b736f35d
hsun <hsun924@sina.com>
2018-05-20 11:26:46 +0800

    增加用户名密码

--- src/main/java/com/yc/sandfactory/entity/User.java
+++ src/main/java/com/yc/sandfactory/entity/User.java
@@ -29,6 +29,18 @@ public class User implements Serializable {
     private String name;
 
     /**
+     * 用户名
+     */
+    @Column("c_username")
+    private String username;
+
+    /**
+     * 密码
+     */
+    @Column("c_password")
+    private String password;
+
+    /**
      * 职位
      */
     @Column("c_position")
@@ -108,6 +120,22 @@ public class User implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getPosition() {
